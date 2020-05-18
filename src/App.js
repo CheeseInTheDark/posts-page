f74d0bed595a94cfac652f65608e3fe024901cc0
Sandy Arendt <sandra.arendt@webcattechnology.com>
2020-05-17 20:33:17 -0400

    Mom: reverted App.js to previous commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-import './index.css'; 
-import AddPost from './AddPost';
+import './index.css';
 
 function App() {
-  const element = <AddPost name="Sara" />;
-    ReactDOM.render(
-    element,
-   document.getElementById('new')
-  )
   return (
-    <div className="App"> 
-        <div id="leftColumn">         
+    <div className="App">
+      <div id="root">
+        <div id="leftColumn"> 
         </div>
         <div className="centerDiv">
           <div id="loginBox">
@@ -32,7 +26,8 @@ function App() {
             </p>
             <button id="login">GO!</button>
           </div>
-        </div> 
+        </div>
+      </div>
     </div>
   );
 }
