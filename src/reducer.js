ce05785313cf6f9bc767f21c0040968087544340
CheeseInTheDark <blockbattle@gmail.com>
2020-05-20 00:01:27 -0400

    [Jeff] Add actions for login failure and set token

--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,14 @@ const INITIAL_STATE = {
 module.exports = function reduce(state = INITIAL_STATE, action) {
     switch (action.type) {
         case 'LOAD_POSTS': 
-          return Object.assign({}, state, { posts: action.value}) 
+            return Object.assign({}, state, { posts: action.value }) 
+        case 'SET_TOKEN':
+            return Object.assign({}, state, {
+                 token: action.value,
+                 loginFailed: false
+            }) 
+        case 'LOGIN_FAILED':
+            return Object.assign({}, state, { loginFailed: true })
     }
     return state
 }
--- src/test/reducer.test.js
+++ src/test/reducer.test.js
@@ -2,9 +2,38 @@ describe("reducer", () => {
 
     const subject = require('../reducer')
 
-    test("returns initial state with no posts in it", () => {
+    const previousState = { oldValue: "old" }
+
+    test("returns initial state", () => {
         const state = subject(undefined, {type: undefined})
 
-        expect(state).toEqual({posts: []})
+        expect(state).toEqual({
+            posts: [],
+            token: undefined
+        })
+    })
+
+    test("SET_TOKEN sets token and login failed state", () => {
+        const state = subject(previousState, {
+            type: "SET_TOKEN",
+            value: "token"
+        })
+
+        expect(state).toEqual({
+            oldValue: "old",
+            loginFailed: false,
+            token: "token"
+        })
+    })
+
+    test("LOGIN_FAILED sets login failed state", () => {
+        const state = subject(previousState, {
+            type: "LOGIN_FAILED"
+        })
+
+        expect(state).toEqual({
+            oldValue: "old",
+            loginFailed: true
+        })
     })
 })
