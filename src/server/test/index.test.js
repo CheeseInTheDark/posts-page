f973c6c65b04989c51c92582fe1af3a8c0e918f1
Sandy Arendt <sandra.arendt@webcattechnology.com>
2020-05-20 21:06:28 -0400

    [Jeff/mom] Add token validation to POST /post, still need to flesh out timestamp, settings, and token-store modules though

--- src/server/test/index.test.js
+++ src/server/test/index.test.js
@@ -1,4 +1,22 @@
 import request from 'supertest'
+import fs from 'fs'
+
+jest.mock('../timestamp', () => () => "timestamp")
+
+jest.mock('../settings', () => ({
+    port: 5001,
+    staticDirectory: "./temp",
+}))
+
+jest.mock('../token-store', () => {
+    let acceptedTokens = []
+    
+    return {
+        reset: () => acceptedTokens = [],
+        add: token => acceptedTokens.push(token),
+        isValid: token => acceptedTokens.indexOf(token) > -1
+    }
+})
 
 jest.mock('uid-generator', () => function UIDGenerator() {
     return {
@@ -8,20 +26,64 @@ jest.mock('uid-generator', () => function UIDGenerator() {
 })
 
 describe("app", () => {
+    
+    let tokenStore
     let subject
     
     beforeEach(async () => {
+        tokenStore = require('../token-store')
+        fs.writeFileSync('./test-file', "test")
+
         subject = await require('../index.js')
+        fs.mkdirSync('./temp/posts', { recursive: true })
     })
     
     afterEach(done => {
+        fs.rmdirSync('./temp', { recursive: true, force: true })
+
         subject.close(() => {
             jest.resetModules()
             done()
         })
     })
     
-    describe("auth", () => {
+    describe("POST /post", () => {
+        test("creates a post when given valid token", async () => {
+            tokenStore.add("token")
+
+            await request(subject)
+                .post('/post')
+                .field("visitorMessage", "hi")
+                .field("visitorName", "Joe")
+                .set('Authorization', "token")
+                .attach("file", './test-file')
+
+            const postMessage = JSON.parse(fs.readFileSync("./temp/posts/1/message.txt").toString())
+
+            expect(postMessage).toEqual({
+                name: "Joe",
+                text: "hi",
+                timestamp: "timestamp",
+                image: "/posts/1/test-file"
+            })
+
+            expect(fs.existsSync("./temp/posts/1/test-file")).toBe(true)
+        })
+
+        test("does not create a post when given a bad token", async () => {
+            const response = await request(subject)
+                .post('/post')
+                .field("visitorMessage", "hi")
+                .field("visitorName", "Joe")
+                .set('Authorization', "token")
+                .attach("file", './test-file')
+
+            expect(fs.existsSync("./temp/posts/1")).toBe(false)
+            expect(response.status).toBe(403)
+        })
+    })
+
+    describe("POST /auth", () => {
         test("auth returns 403 if password is wrong", async () => {
             const response = await request(subject)
                 .post('/auth')
